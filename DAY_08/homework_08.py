d7241c053881801fe8e1f52f645065f632f3742d
spreadmesh <spreadmesh@gmail.com>
2016-05-05 09:20:43 +0900

    Add a homework file
    첫 번째 과제를 업로드 하였습니다.
    숙제 자체가 python 파일이므로 그대로 실행할 수 있습니다.

---
+++ DAY_08/homework_08.py
@@ -0,0 +1,46 @@
+#1번 문제
+print("1.\n")
+
+for i in range(5):
+    print("*" * (i+1))
+
+print("\n")
+
+
+#2번 문제
+print("2.\n")
+
+for i in range(5):
+    print(' ' * (5-i-1) + "*" * (i+1))
+
+print("\n")
+
+
+
+#3번 문제
+print("3.\n")
+
+for i in range(5):
+    print("*" * (5-i))
+
+print("\n")
+
+
+
+
+#4번 문제
+print("4.\n")
+
+for i in range(5):
+    print(' ' * (i) + "*" * (5-i))
+
+print("\n")
+
+
+
+
+
+
+
+
+
