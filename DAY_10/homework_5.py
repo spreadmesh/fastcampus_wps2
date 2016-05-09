4939cab6fca2ec1a19334311af9bc0889cdda603
spreadmesh <spreadmesh@gmail.com>
2016-05-10 02:25:05 +0900

    refactor: Add Dynamic Programming Code
    다이나믹 코드에 대한 내용을 추가

--- DAY_10/homework_5.py
+++ DAY_10/homework_5.py
@@ -7,7 +7,21 @@ def fibonacci(num):
     else:
         return fibonacci(num - 1) + fibonacci(num - 2)
 
-print("fibonacci(2) = {}".format(fibonacci(2)))
-print("fibonacci(3) = {}".format(fibonacci(3)))
-print("fibonacci(4) = {}".format(fibonacci(4)))
-print("fibonacci(5) = {}".format(fibonacci(5)))
+# print("fibonacci(2) = {}".format(fibonacci(2)))
+# print("fibonacci(3) = {}".format(fibonacci(3)))
+# print("fibonacci(4) = {}".format(fibonacci(4)))
+# print("fibonacci(5) = {}".format(fibonacci(5)))
+
+fib_list = [0, 1]
+def fibonacci_dp(x):
+    if x <= 0:
+        return 0
+    if x == 1:
+        return 1
+    if len(fib_list)-1 >= x:
+        return memo[x]
+    if len(fib_list)-1 < x:
+        new_number = fibonacci_dp(x - 1) + fibonacci_dp(x - 2)
+        fib_list.append(new_number)
+        return new_number
+
